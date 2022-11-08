1e780e10ef0c7afe7e8c338d63c3bc2f81016b36
Furkan <91071694+furkanunl11@users.noreply.github.com>
2022-11-08 10:44:49 +0300

    update

--- geziapp/geziapp/geziler/urls.py
+++ geziapp/geziapp/geziler/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
 
-    path("index.html", views.index),
+    path("index", views.index),
     path("geziler", views.geziler),
     path("gezidetay/<slug:slug>", views.gezidetay)
 ]

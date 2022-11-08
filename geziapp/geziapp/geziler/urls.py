6730be6f018893a3a953c13d248e096746a86aa0
Furkan <91071694+furkanunl11@users.noreply.github.com>
2022-11-08 17:18:20 +0300

    statik yapma

--- geziapp/geziapp/geziler/urls.py
+++ geziapp/geziapp/geziler/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
 
-    path("index", views.index),
+    path("", views.index),
     path("geziler", views.geziler),
     path("gezidetay/<slug:slug>", views.gezidetay)
 ]

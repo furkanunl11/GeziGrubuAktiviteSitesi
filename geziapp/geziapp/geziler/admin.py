f213041485ba728fa4bb72c053af2c6d620ddab5
Furkan <91071694+furkanunl11@users.noreply.github.com>
2022-11-17 19:41:40 +0300

    Creating the admin panel...

--- geziapp/geziapp/geziler/admin.py
+++ geziapp/geziapp/geziler/admin.py
@@ -1,2 +1,19 @@
 from django.contrib import admin
+from geziler.models import Setting, User,Profile,Comment,Category,Content,image,faq,attend,message
+from django.contrib.admin.sites import NotRegistered
 
+admin.site.register(Setting)
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Content)
+#admin.site.register(Content)
+admin.site.register(image)
+admin.site.register(faq)
+admin.site.register(attend)
+admin.site.register(message)
+
+# Kullanıcı Adı: admin
+# eposta: unal90182@gmail.com
+# Password: 123
